2ad528455d6f717db65981b31cc9b35285e51f55
Anh Huynh <66988477+vampireplus123@users.noreply.github.com>
2025-05-28 17:10:11 +0700

    update

--- POS_Restaurant/HomePage.cpp
+++ POS_Restaurant/HomePage.cpp
@@ -1,5 +1,5 @@
-#include "mainwindow.h"
-#include "ui_mainwindow.h"
+#include "HomePage.h"
+#include "ui_HomePage.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
--- POS_Restaurant/HomePage.h
+++ POS_Restaurant/HomePage.h
@@ -1,5 +1,5 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+#ifndef HOMEPAGE_H
+#define HOMEPAGE_H
 
 #include <QMainWindow>
 
@@ -20,4 +20,4 @@ public:
 private:
     Ui::MainWindow *ui;
 };
-#endif // MAINWINDOW_H
+#endif // HOMEPAGE_H
